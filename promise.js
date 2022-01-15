a7841cc0117aa9479b9c2fb8921b950323b654ce
pankajrajputiet <95834257+pankajrajputiet@users.noreply.github.com>
2022-01-15 20:33:50 +0530

    Create promise.js

---
+++ promise.js
@@ -0,0 +1,11 @@
+const promisiObj = new Promise((resolve,reject)=>{
+    let condition = false;
+    if(condition == true){
+        resolve("request succesful");
+    }else{
+        reject("request failed");
+    }
+});
+promisiObj.then(
+    (result)=>{console.log(result);
+    }).catch((error)=>{console.log(error)})
